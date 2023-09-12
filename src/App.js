d267be953ba24c4b58148fe3e077c44bfb064c43
kelshardy <14024@coderacademy.edu.au>
2023-09-12 20:51:30 +1000

    fruit list in D.R.Y JSX component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import FruitList from './FruitList';
+
+// some comment here
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <ul>
+        {/* Array of li tags with fruit names as contents */}
+        {/* Use custom JSX element */}
+        <FruitList />
+      </ul>
+    </div>
+  );
+}
+
+export default App;
